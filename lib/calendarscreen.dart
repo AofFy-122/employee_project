3974cfe945254fb9f2899184ed2715ec74010a36
Tinnapop <tinnapop122@gmail.com>
2025-04-23 16:01:56 +0700

    first commit

---
+++ lib/calendarscreen.dart
@@ -0,0 +1,154 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:employee_project/model/user.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class CalendarScreen extends StatefulWidget {
+  const CalendarScreen({super.key});
+
+  @override
+  State<CalendarScreen> createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  double screenHeight = 0;
+  double screenWidth = 0;
+  Color primary = const Color.fromARGB(250, 5, 104, 253);
+
+  @override
+  Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Container(
+                alignment: Alignment.centerLeft,
+                margin: const EdgeInsets.only(top: 32),
+                child: Text(
+                  "My Attendance",
+                  style: TextStyle(
+                    color: Colors.black38,
+                    fontFamily: "Nexa Blod",
+                    fontSize: screenWidth / 18,
+                  ),  
+                ),
+              ),
+            Stack(
+              children: [
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin: const EdgeInsets.only(top: 32),
+                  child: Text(
+                    DateFormat('MMMM').format(DateTime.now()),
+                    style: TextStyle(
+                      fontFamily: "Nexa Blod",
+                      fontSize: screenWidth / 18,
+                    ),  
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerRight,
+                  margin: const EdgeInsets.only(top: 32),
+                  child: Text(
+                    "Pick a Month",
+                    style: TextStyle(
+                      fontFamily: "Nexa Blod",
+                      fontSize: screenWidth / 18,
+                    ),  
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              height: screenHeight - screenHeight /5,
+              child: StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance.collection("Employee").doc(User.id).collection("Record").snapshots(), 
+                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if(snapshot.hasData){
+                    final snap = snapshot.data!.docs;
+                    return ListView.builder(
+                      itemCount: snap.length,
+                      itemBuilder: (context, index){
+                        return Container(
+                          height: 150,
+                          margin: const EdgeInsets.only(top: 12,left: 6,right: 6),
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black26,
+                                blurRadius: 10,
+                                offset: Offset(2, 2),
+                              )
+                            ],
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Text("Check In",
+                                      style: TextStyle(
+                                        fontFamily: "Nexa Light",
+                                        fontSize: screenWidth / 20,
+                                        color: Colors.black54,
+                                      ),
+                                    ),
+                                    Text(snap[index]['checkIn'],
+                                      style: TextStyle(
+                                        fontFamily: "Nexa Bold",
+                                        fontSize: screenWidth / 18,
+                                        color: Colors.black54,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Text("Check Out",
+                                    style: TextStyle(
+                                        fontFamily: "Nexa Light",
+                                        fontSize: screenWidth / 20,
+                                        color: Colors.black54,
+                                      ),
+                                    ),
+                                    Text(snap[index]['checkOut'],
+                                      style: TextStyle(
+                                        fontFamily: "Nexa Bold",
+                                        fontSize: screenWidth / 18,
+                                        color: Colors.black54,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                  }else {
+                    return const SizedBox();
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:employee_project/homescreen.dart';
+import 'package:employee_project/login_screen.dart';
+import 'package:employee_project/model/user.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future <void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const KeyboardVisibilityProvider(
+        child: AuthCheck(),
+      ),
+    );
+  }
+}
+
+
+
+class AuthCheck extends StatefulWidget {
+  const AuthCheck({super.key});
+
+  @override
+  State<AuthCheck> createState() => _AuthCheckState();
+}
+
+class _AuthCheckState extends State<AuthCheck> {
+  bool userAvailable = false;
+  late SharedPreferences sharedPreferences;
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentUser();
+  }
+
+  void _getCurrentUser() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+
+    try {
+      if(sharedPreferences.getString("employeeId") != null){
+        setState(() {
+          User.employeeId = sharedPreferences.getString("employeeId")!;
+          userAvailable = true;
+        });
+      }
+    } catch (e) {
+      setState(() {
+          userAvailable = false;
+        });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return userAvailable ? const HomeScreen() : const LoginScreen();
+  }
+}
+
+
